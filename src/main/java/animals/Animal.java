0311917dfbb3a64108c1fc3b30457892ebaceca0
Dakoog <taktoja@spoko.pl>
2022-06-13 15:49:11 +0200

    adding two classes Dog and Cat which extend class Animal and modify Animal - formatting

--- src/main/java/animals/Animal.java
+++ src/main/java/animals/Animal.java
@@ -1,12 +1,14 @@
 package animals;
 
 public abstract class Animal {
-   private final String name;
 
-   public Animal(String name) {
-      this.name = name;
-   }
-  public abstract void saySomething(); // abstract method -> haven't got implementation
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract void saySomething(); // abstract method -> haven't got implementation
 
 
 }
---
+++ src/main/java/animals/Cat.java
@@ -0,0 +1,13 @@
+package animals;
+
+public class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void saySomething() {
+
+    }
+}
