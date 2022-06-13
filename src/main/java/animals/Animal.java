4e85e762e8685df95e43cf0c4c8112474f996b1c
Dakoog <taktoja@spoko.pl>
2022-06-13 15:37:54 +0200

    adding "animals" package with "Animal" abstract class.

---
+++ src/main/java/animals/Animal.java
@@ -0,0 +1,12 @@
+package animals;
+
+public abstract class Animal {
+   private final String name;
+
+   public Animal(String name) {
+      this.name = name;
+   }
+  public abstract void saySomething(); // abstract method -> haven't got implementation
+
+
+}
