362c2e18f0c910c8418aeb1f0f3be499feb5f4c0
Dakoog <taktoja@spoko.pl>
2022-06-15 14:17:08 +0200

    formatting Runner interface

--- src/main/java/animals/Runner.java
+++ src/main/java/animals/Runner.java
@@ -1,7 +1,26 @@
 package animals;
 
 public interface Runner {
-    String name = "runner";//public static final (fields).
+
+
+
     void run();// and public abstract (methods) is default
+    default void count() {// the default method requires implementation in the interface, is inherited by classes
+                         // that implement the Runner interface and can be overridden
+        showPrivateCounting();
+        run();                                  // wywołanie metody run() z klasy implementującej interfejs Runner
+    }
+
+    static void staticCount() {                 // static method requires implementation in the interface, cannot be overridden, can exit of object -> Runner.staticCount()
+        showPrivateStaticCounting();
+    }
+
+    private void showPrivateCounting() {        // private method requires implementation in the interface,
+        System.out.println("3, 2, 1...start!");
+    }
+
+    private static void showPrivateStaticCounting() {   // private static method requires implementation in the interface,
+        System.out.println("3, 2, 1...start!");
+    }
 
 }
