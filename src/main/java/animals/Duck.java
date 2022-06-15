801a36136fc0d1b843e712e8a48da787a5577b1c
Dakoog <taktoja@spoko.pl>
2022-06-15 14:15:45 +0200

    adding inner class (Duckling) for Duck class, with her reference

--- src/main/java/animals/Duck.java
+++ src/main/java/animals/Duck.java
@@ -1,13 +1,25 @@
 package animals;
 
-public class Duck extends Bird{
+public class Duck extends Bird {
 
-    public Duck(String name) {
+    public Duckling duckling;
+
+    public Duck(String name, Duckling duckling) {
         super(name);
+        this.duckling = new Duckling();// instate for inner class Duckling
     }
 
     @Override
     public void saySomething() {
+        System.out.println(name + ": Kwa kwa");
+    }
 
+    public class Duckling {
+        public void say() {
+            System.out.println("Duckling: " + name + " this is my uncle");   //the inner class has access of fields and methods class Duck
+        }
     }
 }
+
+
+
