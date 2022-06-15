2203df3d94c574ddee331d0b530e33dd28418349
Dakoog <taktoja@spoko.pl>
2022-06-15 14:16:16 +0200

    modify Animal class - formatting code

--- src/main/java/animals/Animal.java
+++ src/main/java/animals/Animal.java
@@ -2,7 +2,7 @@ package animals;
 
 public abstract class Animal { // can't create instantiate for abstract class
 
-    protected String name;// protected because "name" field is used in classes which extend Animal class
+   protected String name;// protected because "name" field is used in classes which extend Animal class
 
     public Animal(String name) {
         this.name = name;
