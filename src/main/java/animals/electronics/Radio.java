6d0276b17e7a8124100257cd5fbdad5024e319d4
Dakoog <taktoja@spoko.pl>
2022-06-18 18:24:32 +0200

    adding  Radio class which extends Device class

---
+++ src/main/java/animals/electronics/Radio.java
@@ -0,0 +1,19 @@
+package animals.electronics;
+
+public class Radio extends Device{
+    public Radio(String model) {
+        super(model);
+    }
+    public void changeChannel(String channel){
+        System.out.println("Changing to a new radio channel: "+ channel);
+    }
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+}
