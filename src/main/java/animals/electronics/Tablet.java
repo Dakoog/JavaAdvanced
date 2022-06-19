45a89533f254f1003b6d361d7bfc387d47b5a28f
Dakoog <taktoja@spoko.pl>
2022-06-19 12:26:49 +0200

    adding Tablet class extends Device with sendMessage method.

---
+++ src/main/java/animals/electronics/Tablet.java
@@ -0,0 +1,22 @@
+package animals.electronics;
+
+public class Tablet extends Device {
+
+    public Tablet(String model) {
+        super(model);
+    }
+
+    public void sendMessage(String type, String message) {
+        System.out.println();
+    }
+
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+}
