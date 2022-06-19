061025a9f4a6f316d9828478eee1fa0a085b693b
Dakoog <taktoja@spoko.pl>
2022-06-19 12:57:29 +0200

    First step: implements the EmailSender interface for the Tablet class Second step: overrides all methods from the abstract class (Device) and the interface (EmailSender)

--- src/main/java/animals/electronics/Tablet.java
+++ src/main/java/animals/electronics/Tablet.java
@@ -1,22 +1,30 @@
 package animals.electronics;
 
-public class Tablet extends Device {
+public class Tablet extends Device implements EmailSender {
 
     public Tablet(String model) {
         super(model);
     }
 
     public void sendMessage(String type, String message) {
-        System.out.println();
+        String consoleMessage = String.format("I send message type %s z %s: %s", type, model, message);
+        System.out.println(consoleMessage);
     }
 
     @Override
     public void turnOn() {
-
+        System.out.println("Tablet "+ model +" is turn on");
     }
 
     @Override
     public void turnOff() {
+        System.out.println("Tablet "+ model +" is turn off");
 
     }
+
+    @Override
+    public void sendEmail(String message) {
+        sendMessage("email",message);
+    }
+
 }
