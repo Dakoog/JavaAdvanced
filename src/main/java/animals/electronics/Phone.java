5f6c9cf760d60bd239c9d2d0d7c207c8fca5fb23
Dakoog <taktoja@spoko.pl>
2022-06-19 11:33:12 +0200

    adding Phone class extends Device with MakeCall method.

---
+++ src/main/java/animals/electronics/Phone.java
@@ -0,0 +1,23 @@
+package animals.electronics;
+
+public class Phone extends Device {
+    private String number;
+
+    public void makeCall(String number) {
+        System.out.println("I'm calling the number: " + number);
+    }
+
+    public Phone(String model) {
+        super(model);
+    }
+
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+}
