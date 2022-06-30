c37bcfe97d47f0d57aaf313fd53b5c7e9a31ea61
Dakoog <taktoja@spoko.pl>
2022-06-30 12:06:36 +0200

    adding MailMachine contains composition with EmailSender interface.

--- src/main/java/electronics/MailMachine.java
+++ src/main/java/electronics/MailMachine.java
@@ -1,8 +1,25 @@
 package electronics;
 
-public class MailMachine implements EmailSender {
-    @Override
+public class MailMachine  {
+    private static final int MAX_ELEMENTS =4;
+    //setting the limit of sent messages
+    private final EmailSender[] senders = new EmailSender[MAX_ELEMENTS];
+    //entering a counter that counts all send messages by this device
+    private int counter = 0;
+    //method - which adds a new message to be sent
+    public void addEmailSender(EmailSender emailSender){
+        if(counter > MAX_ELEMENTS -1){
+            System.out.println("Can't add new message to be send");
+        }
+        senders[counter] = emailSender;
+        counter ++;
+        System.out.println("Adding new message (emailSender)");
+    }
+    // composition - adding a method that will send a new message via the EmailSender interface.
     public void sendEmail(String message) {
-        
+        for (EmailSender emailSender : senders) {
+            emailSender.sendEmail(message);
+        }
     }
 }
+
