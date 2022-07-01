6d2dce4978ff9fa4492573add5f5d74739c01455
Dakoog <taktoja@spoko.pl>
2022-07-01 09:31:05 +0200

    implementation of EmailSender interface for Phone class

--- src/main/java/electronics/Phone.java
+++ src/main/java/electronics/Phone.java
@@ -1,14 +1,14 @@
 package electronics;
 
-public class Phone extends Device {
+public class Phone extends Device implements EmailSender {
     private String number;
+
+    public Phone(String model, String number) {
+        super(model);
+    }
 
     public void makeCall(String number) {
         System.out.println("I'm calling the number: " + number);
-    }
-
-    public Phone(String model) {
-        super(model);
     }
 
     @Override
@@ -19,5 +19,10 @@ public class Phone extends Device {
     @Override
     public void turnOff() {
 
+    }
+
+    @Override
+    public void sendEmail(String message) {
+        System.out.println("Email sender via "+model+": "+ message);
     }
 }
