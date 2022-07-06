51032ce162a3851462818f465112b37b972fe453
Dakoog <taktoja@spoko.pl>
2022-07-06 10:20:01 +0200

    adding Gender Enum for collections package

---
+++ src/main/java/collections/Gender.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.nio.file.Watchable;
+
+public enum Gender {
+    M("MAN"),W("WOMAN");
+    String gender;
+
+    Gender(String gender) {
+        this.gender = gender;
+    }
+
+
+}
