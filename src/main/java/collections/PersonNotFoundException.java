a448ed269015f951f73ffa4f41d0f19cb7846606
Dakoog <taktoja@spoko.pl>
2022-07-06 10:23:38 +0200

    adding PhoneBook class with PersonNotFoundException exception.

--- src/main/java/collections/PersonNotFoundException.java
+++ src/main/java/collections/PersonNotFoundException.java
@@ -1,7 +1,7 @@
 package collections;
 
 public class PersonNotFoundException extends Exception {
-   public PersonNotFoundException (String number){
-       super("The person with the number "+ number +" is not in the phone book.");
-   }
+    public PersonNotFoundException(String number) {
+        super("The person with the number " + number + " is not in the phone book.");
+    }
 }
